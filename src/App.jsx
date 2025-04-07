18b21f57fd5454ac4c2b57c66dbd67f9460340f5
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-04-07 10:53:02 +0600

    PH-B11-React-SPA-Project-json-data-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Blogs from './Components/Blogs/Blogs'
 
 function App() {
 
   return (
     <>
      <Navbar></Navbar>
+     <Blogs></Blogs>
     </>
   )
 }
 
 export default App
+ 
